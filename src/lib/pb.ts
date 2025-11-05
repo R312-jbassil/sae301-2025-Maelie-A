40dd17577ef8ebfd96074ea487876b2dc646a41c
MaëlieA <maelie@addario.fr>
2025-11-05 15:35:20 +0100

    feat: Add PocketBase integration with environment-based configuration

---
+++ src/lib/pb.ts
@@ -0,0 +1,12 @@
+import PocketBase from "pocketbase";
+
+type TypedPocketBase = PocketBase & {
+  // extend with custom collection types or helper methods if needed
+};
+
+var path = "";
+if (import.meta.env.MODE === "development")
+  path = "http://localhost:8090"; //localhost = machine de dev
+else path = "http://localhost:8091"; //localhost = machine de déploiement
+const pb = new PocketBase(path) as TypedPocketBase;
+export default pb;
